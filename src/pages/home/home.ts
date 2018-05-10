dd847b06fe349d62e50b4063d8f5724628ffd421
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-05-10 17:19:10 -0300

    se agrega texto de bienvenida se cambia el estilo del botón cerrar session

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ImageSlidePage } from '../image-slide/image-slide';
 import { PhotoTakerPage } from '../photo-taker/photo-taker';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 import { LoginPage } from '../login/login';
+import { User } from '@firebase/auth-types';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-  test:number;
+export class HomePage implements OnInit {
+
+  user: User;
   constructor(public navCtrl: NavController, private auth: AuthServiceProvider) {
-    this.test = 0;
+    
   }
 
-  panned(){
-    this.test++;
-    console.log(this.test);
+  ngOnInit(): void {
+    this.user = this.auth.getUserInfo();
   }
 
   cosasFeas(){
