ea395bb620081c1db9e88b747e40a62fb9c3f3ea
Lucas Bonanni <lucas_eb2004@yahoo.com.ar>
2018-05-25 23:13:52 -0300

    se ordenan los imports de los módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,48 +7,58 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { IonicImageViewerModule } from 'ionic-img-viewer';
 import { Camera } from '@ionic-native/camera';
+import { DeviceMotion } from '@ionic-native/device-motion';
 
 //App modules
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ImageSlidePage } from '../pages/image-slide/image-slide';
-import { PhotoTakerPage } from '../pages/photo-taker/photo-taker';
+// import { ImageSlidePage } from '../pages/image-slide/image-slide';
+// import { PhotoTakerPage } from '../pages/photo-taker/photo-taker';
+// import { LoginPage } from '../pages/login/login';
 import { AuthServiceProvider } from '../providers/auth-service/auth-service';
 //firebase
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { firebaseConfig } from '../enviroments/enviroment';
 import { AngularFireModule } from 'angularfire2';
-import { LoginPage } from '../pages/login/login';
+
 import { ImageDbProvider } from '../providers/firebase/firebase';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { BusyLoaderProvider } from '../providers/busy-loader/busy-loader';
+
+import { ImageSlidePageModule } from '../pages/image-slide/image-slide.module';
+import { LoginPageModule } from '../pages/login/login.module';
+import { PhotoTakerPageModule } from '../pages/photo-taker/photo-taker.module';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ImageSlidePage,
-    PhotoTakerPage,
-    LoginPage
+    // ImageSlidePage,
+    // PhotoTakerPage,
+    // LoginPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireAuthModule,
-    IonicImageViewerModule
+    IonicImageViewerModule,
+    ImageSlidePageModule,
+    LoginPageModule,
+    PhotoTakerPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ImageSlidePage,
-    PhotoTakerPage,
-    LoginPage
+    // ImageSlidePage,
+    // PhotoTakerPage,
+    // LoginPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    DeviceMotion,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     Camera,
     AngularFireDatabase,
